6d79de4c6e0a51c612fd906b16c972f528693426
Rohit Sharma <lavish637728@gmail.com>
2024-11-23 23:56:15 +0530

    server setup

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,33 @@
+const cookieParser = require('cookie-parser');
 const express = require('express');
+const mongoose = require ('mongoose');
+const cors = require('cors');
 
-const app = express({
-    
-});
+
+mongoose.connect('mongodb+srv://lavish637728:lavish637728@sachinecom.uragf.mongodb.net/').then(()=>
+    console.log("connected to database")).catch(error =>(console.log(error))
+);
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(
+    cors({
+        origin:' http://localhost:5173/',
+        methods:['GET ','POST', 'DELETE' , 'PUT'],
+        allowedHeaders:[
+            "Content-type",
+            "Authrization",
+            "Cache-Control",
+            'Expires',
+            'Pragma'
+        ],
+        credentials: true,
+    })
+);
+
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.listen(PORT , ()=>console.log(`server is running on ${PORT}`))
